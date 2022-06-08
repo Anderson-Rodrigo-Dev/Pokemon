425fb134028710cbe69fa0343531ab9832752a8e
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-06-07 23:28:09 -0300

    subindo repositório com a lista dos 150 pokemons iniciais

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-const redPokemons = []
+const   quantidadePokemons = 150
+let     pokemonsVisiveis = 15
+
+document.querySelector("#btn").addEventListener('click', function () {
+    
+
+    acessandoPokemon()
+})
 
 
 async function acessandoPokemon() {
-    const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150"
-    const pokemons = await fetch(url)
-    const listaPokemons = await pokemons.json()
-    const resultado = listaPokemons.results
+    const pokemons = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    const listaPokemons = []
 
-    resultado.forEach(pokemon => {
-        redPokemons.push(pokemon.name)
+    
+    for(let i = 1; i <= quantidadePokemons; i++){
+        const lista = await fetch(pokemons(i))
+        listaPokemons.push(await lista.json())
+    }
+    
+    //Ordena os Pokemons em ordem alfabetica
+
+    /* listaPokemons.sort(function(a,b) {
+        return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     });
+
+    listaPokemons.splice(pokemonsVisiveis) */
+
+    
+
+    const list = listaPokemons.reduce((acumulador, pokemon) =>{
+        const tipos = pokemon.types.map(typeInfo => typeInfo.type.name)
+        acumulador += `
+            <li class="card ${tipos[0]}">
+                <img class="card-imagem" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card-subtitle">${tipos.join(" | ")}</p>
+            </li>
+        `
+        return acumulador
+    },'')
+
+    const ul = document.querySelector('[data-js="pokedex"]')
+
+    ul.innerHTML = list
 }
 
 acessandoPokemon()
-
-setTimeout(() => {
-    const novosPokemons = redPokemons.filter(pokemons => pokemons.charAt(0) == 'h')
-
-    console.log(novosPokemons)
-}, 100);
